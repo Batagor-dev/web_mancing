fd13591f41eb1eb7a33bbe901a713f98baec5caf
Kann <kaniabudiman14@gmail.com>
2025-12-09 14:06:11 +0700

    galeri

--- app/Models/Galery.php
+++ app/Models/Galery.php
@@ -3,8 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 
 class Galery extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'galeries';
+
+    protected $fillable = [
+        'uuid',
+        'slug',
+        'photo',
+        'title',
+        'time',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($galery) {
+            // generate uuid jika belum ada
+            if (empty($galery->uuid)) {
+                $galery->uuid = (string) Str::uuid();
+            }
+
+            // generate slug otomatis dari title jika belum ada
+            if (empty($galery->slug) && !empty($galery->title)) {
+                $galery->slug = Str::slug($galery->title);
+            }
+        });
+    }
 }
